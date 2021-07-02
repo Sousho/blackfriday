c18423b769a2dd1522866c154bae7f68473e8331
JohnPiwinski <johnpiwinski@protonmail.ch>
2021-07-02 15:17:06 -0500

    update

--- sousho.go
+++ sousho.go
@@ -39,6 +39,7 @@ func init_math() {
 
 func render_shorthand(eq []byte) []byte {
 	eq_str := strings.Trim(string(eq), " ")
+	eq_str = strings.Replace("\n", "")
 	if eq_str == "" {
 		return []byte("$$")
 	}
