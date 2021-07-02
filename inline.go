4168894039973908e9e853f96ce536b98b5c804a
JohnPiwinski <johnpiwinski@protonmail.ch>
2021-07-02 01:04:09 -0500

    update

--- inline.go
+++ inline.go
@@ -55,6 +55,14 @@ func (p *Markdown) inline(currBlock *Node, data []byte) {
 	beg, end := 0, 0
 	for end < len(data) {
 		handler := p.inlineCallback[data[end]]
+		// A simple patch to correct the fact that no 2 byte runes exist
+		if end >= 1 && data[end-1] == '$' && data[end] == '$' {
+			handler = func(p *Markdown, data []byte, end int) (int, *Node) {
+				consumed, node := p.inlineCallback[data[end]](p, data, end)
+				node.Literal = []byte("$" + string(node.Literal) + "$")
+				return consumed + 2, node
+			}
+		}
 		if handler != nil {
 			if consumed, node := handler(p, data, end); consumed == 0 {
 				// No action from the callback.
